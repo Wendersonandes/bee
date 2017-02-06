2d94b730580037d62815d3698285aad76f4e3715
lucas <lucasfneves14@gmail.com>
2017-02-06 17:09:40 -0200

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
+    :domain               => "beeprinted.herokuapp.com",
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
     :authentication       => "plain",
