70e74e258ad67f2fdcd577fc0ff86b20851aa8c2
lucas <lucasfneves14@gmail.com>
2017-07-31 19:08:55 -0300

    make it better

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -30,7 +30,7 @@ class RegistrationsController < Devise::RegistrationsController
     end
   end
   def after_sign_out_path_for(users)
-    ''
+    galeria_path
   end
   def after_update_path_for(resource)
       current_user.update_attribute(:completo, "#{resource.name} #{resource.sobrenome}")
