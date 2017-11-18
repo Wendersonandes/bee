cf8b91caac0f2d44a59e1e8794c62306f076b77f
lucas <lucasfneves14@gmail.com>
2017-11-18 16:12:43 -0200

    make it better

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -2,12 +2,15 @@ class LearnController < ApplicationController
 	def index
 	end
 	def primeira
+		@user = User.find(6)
 	end
 	def acabamento
 		@user = User.find(6)
 	end
 	def modelagem
+		@user = User.find(6)
 	end
 	def material
+		@user = User.find(6)
 	end
 end
